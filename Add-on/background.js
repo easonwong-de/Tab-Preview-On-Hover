9b98e10312af76150edb7c431b891096adcce1df
Eason Wong <me@easonwong.de>
2023-04-10 22:21:50 +0200

    Update v0.0.1

--- Add-on/background.js
+++ Add-on/background.js
@@ -1,32 +1,39 @@
 function update() {
-	browser.tabs.query({ currentWindow: true }).then((tabs) => {
-		const screenshots = [];
-		for (let i = 0; i < Math.min(10, tabs.length); i++) {
-			screenshots.push(
-				browser.tabs.captureTab(tabs[i].id, {
-					format: "jpeg",
-					quality: 50,
-					scale: 0.5
-				})
-			);
-		}
-		Promise.all(screenshots).then((screenshots) => {
-			browser.theme.getCurrent().then((currentTheme) => {
-				const newTheme = {
-					...currentTheme,
-					images: {
-						...currentTheme.images,
-						additional_backgrounds: screenshots,
-					},
-				};
-				browser.theme.update(currentTheme.windowId, newTheme);
+	setTimeout(() => {
+		browser.windows.getAll({ populate: true }).then((windows) => {
+			windows.forEach((window) => {
+				const tabs = window.tabs;
+				const screenshots = [];
+				for (let i = 0; i < Math.min(10, tabs.length); i++) {
+					screenshots.push(
+						browser.tabs.captureTab(tabs[i].id, {
+							format: "jpeg",
+							quality: 50,
+							scale: 0.5,
+						})
+					);
+				}
+				Promise.all(screenshots).then((screenshots) => {
+					browser.theme.getCurrent(window.id).then((currentTheme) => {
+						const newTheme = {
+							...currentTheme,
+							images: {
+								additional_backgrounds: screenshots,
+							},
+						};
+						browser.theme.update(window.id, newTheme);
+					});
+				});
 			});
 		});
-	});
+	}, 100); // Sets delay to resolve potential conflicts with ATBC
 }
 
-browser.tabs.onUpdated.addListener(update); //When new tab is opened / reloaded
-browser.tabs.onActivated.addListener(update); //When switch tabs
-browser.tabs.onAttached.addListener(update); //When attach tab to windows
-browser.windows.onFocusChanged.addListener(update); //When new window is opened
+browser.tabs.onUpdated.addListener(update); // When new tab is opened / reloaded
+browser.tabs.onActivated.addListener(update); // When switch tabs
+browser.tabs.onAttached.addListener(update); // When attach tab to windows
+browser.tabs.onMoved.addListener(update); // When move tab within a window
+browser.windows.onFocusChanged.addListener(update); // When new window is opened
 browser.runtime.onInstalled.addListener(update);
+
+update();
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Tab-Preview-On-Hover (experimental)
 
-Adds tab preview to Firefox with the help of userChrome.css  
+Adds on-hover tab preview to Firefox (requires CSS theme)  
 
 <img width="1113" alt="HTP_preview" src="https://user-images.githubusercontent.com/16183548/230820629-db83b208-3187-4630-8a9d-126dff550a16.png">
