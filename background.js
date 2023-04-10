5848d466be6e877b0078b7a3cbd34991669f4c1c
Eason Wong <me@easonwong.de>
2023-04-10 05:34:21 +0200

    Update 0.0.1

--- background.js
+++ background.js
@@ -1,41 +1,27 @@
-// When the add-on is installed or upgraded, register a listener to capture tab screenshots
-browser.runtime.onInstalled.addListener(() => {
-	browser.tabs.onUpdated.addListener(handleTabUpdated);
-});
+function update() {
+	browser.tabs.query({ currentWindow: true }).then((tabs) => {
+		console.log(tabs);
+		const promises = tabs.map((tab) =>
+			browser.tabs.captureTab(tab.id, { format: "jpeg", quality: 60, rect: { x: 0, y: 0, width: tab.width, height: tab.width * 0.75 } })
+		);
 
-// When a tab is updated, send a message to the content script to take a screenshot
-async function handleTabUpdated(tabId, changeInfo, tab) {
-	if (changeInfo.status === "complete") {
-		// Get the current theme object
-		const currentTheme = await browser.theme.getCurrent();
-
-		// Capture a screenshot of the updated tab
-		const screenshot = await browser.tabs.captureVisibleTab(tab.windowId, {
-			format: "jpeg",
-			quality: 60,
-			rect: { width: 800, height: 600 },
+		Promise.all(promises).then((screenshots) => {
+			console.log(screenshots);
+			browser.theme.getCurrent().then((currentTheme) => {
+				const newTheme = {
+					...currentTheme,
+					images: {
+						...currentTheme.images,
+						additional_backgrounds: screenshots,
+					},
+				};
+				browser.theme.update(currentTheme.windowId, newTheme);
+			});
 		});
-
-		// Update the theme object with the new screenshot
-		const newTheme = {
-			...currentTheme,
-			images: {
-				...currentTheme.images,
-				additional_backgrounds: [screenshot],
-			},
-		};
-
-		// Update the theme with the new object
-		browser.theme.update(tab.windowId, newTheme);
-	}
+	});
 }
 
-// Listen for messages from content scripts
-browser.runtime.onMessage.addListener(async (message, sender) => {
-	if (message.action === "screenshotTaken") {
-		// Update the theme API with the captured screenshot
-		browser.theme.update(sender.tab.windowId, {
-			additional_backgrounds: [message.screenshot],
-		});
-	}
-});
+browser.tabs.onUpdated.addListener(update); //When new tab is opened / reloaded
+browser.tabs.onActivated.addListener(update); //When switch tabs
+browser.tabs.onAttached.addListener(update); //When attach tab to windows
+browser.windows.onFocusChanged.addListener(update); //When new window is opened
