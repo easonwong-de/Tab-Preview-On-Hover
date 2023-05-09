1bc352ee345e5c445a50546f015c9adbf543b287
Eason Wong <me@easonwong.de>
2023-05-09 20:03:13 +0200

    Update v0.0.2

--- Add-on/background.js
+++ Add-on/background.js
@@ -3,24 +3,30 @@ function update() {
 		windows.forEach((window) => {
 			const tabs = window.tabs;
 			const screenshots = [];
+			const capture_para = {
+				format: "jpeg",
+				quality: 75,
+				scale: 0.5,
+			};
 			for (let i = 0; i < Math.min(10, tabs.length); i++) {
-				screenshots.push(
-					browser.tabs.captureTab(tabs[i].id, {
-						format: "jpeg",
-						quality: 50,
-						scale: 0.5,
-					})
-				);
+				if (tabs[i].status == "complete") {
+					if (tabs[i].discarded) {
+						browser.tabs.reload(tabs[i].id).then(() => {
+							screenshots.push(browser.tabs.captureTab(tabs[i].id, capture_para));
+						});
+					} else {
+						screenshots.push(browser.tabs.captureTab(tabs[i].id, capture_para));
+					}
+				} else {
+					screenshots.push(
+						"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
+					);
+				}
 			}
 			Promise.all(screenshots).then((screenshots) => {
-				browser.theme.getCurrent(window.id).then((currentTheme) => {
-					const newTheme = {
-						...currentTheme,
-						images: {
-							additional_backgrounds: screenshots,
-						},
-					};
-					browser.theme.update(window.id, newTheme);
+				browser.theme.getCurrent(window.id).then((theme) => {
+					theme.images = { additional_backgrounds: screenshots };
+					browser.theme.update(window.id, theme);
 				});
 			});
 		});
@@ -29,4 +35,4 @@ function update() {
 
 browser.runtime.onInstalled.addListener(update);
 
-setInterval(update, 2500);
+setInterval(update, 2000);
